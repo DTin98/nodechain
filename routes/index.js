18a6d91c7345bcf8874b7a664583b2113ba5d535
DTin98 <kaisin1505@gmail.com>
2020-06-01 08:56:30 +0700

    added block, blockchain, write unit test, settup enviroment

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var router = express.Router();
+const P2pserver = require("../p2p-server");
+const Blockchain = require("../blockchain");
+const p2pserver = new P2pserver();
+const blockchain = new Blockchain();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Hello World" });
+});
+
+//GET blockchain
+router.get("/blocks", function (req, res, next) {
+  res.json(blockchain.chain);
+});
+
+//ADD Blocks
+router.post("/mine", function (req, res, next) {
+  const block = blockchain.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
+  res.redirect("/blocks");
+  p2pserver.syncChain();
+});
+
+module.exports = router;
